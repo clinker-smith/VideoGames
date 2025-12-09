7c8eaf57420cbaf0b598cd63b8ccaa945a31303e
dchurch-2027 <daniellehchurch@gmail.com>
2025-12-09 17:00:57 -0500

    Fixed 3 out of 4 errors in calc_time_diff test

--- tests/testthat/test-calc_time_diff.R
+++ tests/testthat/test-calc_time_diff.R
@@ -4,17 +4,32 @@ test_that("calc_time_diff returns time difference", {
     last_update  = as.Date("2020-02-01"))
 
   x <- 1
-  result <- calc_time_diff(.data, x)
+  result <- calc_time_diff(difftime, x)
 
-  expect_s3_class(result, "data.frame")
-  expect_equal(names(result), c(".data", "x"))})
+  expect_s3_class(result, "character")
+  expect_equal(result, "Time difference of 4.428571 weeks")})
 
 test_that("calc_time_diff shows custom error messages when needed", {
+  .data <- data.frame(
+    release_date = as.Date(NA),
+    last_update  = as.Date(NA))
+  x <- 1
+
   expect_error(calc_time_diff(.data, x), "Error: this video game has no listed release or update date.")})
 
 test_that("calc_time_diff shows custom error messages when needed", {
+  .data <- data.frame(
+    release_date = as.Date("2020-01-01"),
+    last_update  = as.Date(NA))
+  x <- 1
+
   expect_error(calc_time_diff(.data, x), "Error: this video game has no listed update date.")})
 
 test_that("calc_time_diff shows custom error messages when needed", {
+  .data <- data.frame(
+    release_date = as.Date(NA),
+    last_update  = as.Date("2020-02-01"))
+  x <- 1
+
   expect_error(calc_time_diff(.data, x), "Error: this video game has no listed release date.")
 })
